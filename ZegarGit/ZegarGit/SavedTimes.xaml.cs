1d4df7ae0f4105f0beb90cb0482362ecd6d4a73a
pawel054 <pawel.marcisz1@gmail.com>
2024-03-19 12:45:29 +0100

    Add SavedTimes page

---
+++ ZegarGit/ZegarGit/SavedTimes.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ZegarGit
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SavedTimes : ContentPage
+    {
+        public SavedTimes()
+        {
+            InitializeComponent();
+        }
+    }
+}
