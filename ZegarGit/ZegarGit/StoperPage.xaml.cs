925c512756efd8f5cab4b6e2cd49439368d43947
pawel054 <pawel.marcisz1@gmail.com>
2024-03-19 13:15:06 +0100

    Add StoperPage

---
+++ ZegarGit/ZegarGit/StoperPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ZegarGit
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StoperPage : ContentPage
+    {
+        public StoperPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
